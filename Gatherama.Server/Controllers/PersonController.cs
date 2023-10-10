49ee8aa2dc753917755a7f5144d035ac6a7e7f8c
Olli <olli.p.mononen@gmail.com>
2023-10-10 12:50:34 +0300

    Sing in toimii, mutta näkymien toteutusta en ole vielä tehnyt. defaulttina onnistuneen ''kirjautumisen myötä'' mennään /ownspots sivulle

--- Gatherama.Server/Controllers/PersonController.cs
+++ Gatherama.Server/Controllers/PersonController.cs
@@ -12,10 +12,12 @@ namespace Gatherama.Server.Controllers
     [Route("api/[controller]")]
     public class PersonController : ControllerBase
     {
+        private readonly ILogger<PersonController> _logger;
         private readonly GatheramaDbContext _context;
 
-        public PersonController(GatheramaDbContext context)
+        public PersonController(ILogger<PersonController> logger, GatheramaDbContext context)
         {
+            _logger = logger;
             _context = context;
         }
 
@@ -23,6 +25,17 @@ namespace Gatherama.Server.Controllers
         public IActionResult GetAll()
         {
             return Ok(_context.Persons.Find(_ => true).ToList());
+        }
+        [HttpPost]
+        public async Task<IActionResult> GetPerson([FromBody] PersonDto person)
+        {
+            var filter = Builders<Person>.Filter.Eq(u => u.username, person.username) & Builders<Person>.Filter.Eq(u => u.password, person.password);
+            var user = await _context.Persons.Find(filter).FirstOrDefaultAsync();
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok();
         }
 /*                public async Task<List<PersonDto>> Get() =>
                     await _personsService.GetAsync();*/
