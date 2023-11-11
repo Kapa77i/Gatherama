b12bade1030aec6c9079968a901af183e16f1c90
Katriina Korhonen <Katriina.Korhonen2@edu.savonia.fi>
2023-11-11 13:05:56 +0200

    Little change in DtoMapper and now public places are also on the map (as mushrooms, but we'll fix it later)

--- Gatherama.Server/Models/DtoMapper.cs
+++ Gatherama.Server/Models/DtoMapper.cs
@@ -9,7 +9,7 @@ namespace Gatherama.Server.Models
             return new FindingDto
             {
                 Id = finding.Id,
-                Private = finding._private,
+                _private = finding._private,
                 datetime = finding.datetime,
                 amount = finding.amount,
                 memo = finding.memo,
