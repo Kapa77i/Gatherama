cdba8cedb5d149794de99ef60f3de7d68586d28a
Katriina Korhonen <Katriina.Korhonen2@edu.savonia.fi>
2023-11-27 19:56:16 +0200

    Added: - [Required] fields to FindingDto, PlaceDto and SpeciesDto for Validation - Some svg images in hopes that android marker error is going to be solved - Validation thingies and bubblegum solutions to AddSpot

--- Gatherama.Shared/SpeciesDto.cs
+++ Gatherama.Shared/SpeciesDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,10 @@ namespace Gatherama.Shared
     public class SpeciesDto
     {
         public string? Id { get; set; }
+        [Required]
         public string? category { get; set; } = null!;
         public string? subCategory { get; set; } = null!;
+        [Required]
         public string? s_name { get; set; } = null!;
         public string? latin_name { get; set; } = null!;
     }
